91cc38a32aa992e02ccf1864104cb1eec7eda630
Dawid8209 <115348369+Dawid8209@users.noreply.github.com>
2024-02-09 18:47:42 +0100

    Add files via upload

---
+++ Calculator/src/main/java/ijd/calculator/Model/Calculator.java
@@ -0,0 +1,9 @@
+package ujd.calculator.Model;
+
+import ujd.calculator.Controller.CalculatorController;
+
+public class Calculator extends CalculatorController {
+    
+    CalculatorController calculator = new CalculatorController();
+    
+}
